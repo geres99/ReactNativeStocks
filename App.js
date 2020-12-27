f41b308d2c27ca5a15fe75eaa44fcea04f26d4e4
Marcin Medryk <medryk.kontakt@gmail.com>
2020-12-27 20:32:06 +0100

    Clean

--- App.js
+++ App.js
@@ -3,100 +3,9 @@ import React from "react";
 import { StyleSheet, Button, Text, View, Dimensions } from "react-native";
 
 export default function App() {
-  let windowHeight = Dimensions.get("window").height;
-  let windowWidth = Dimensions.get("window").width;
-
-  let maxHeight = windowHeight * 0.5 * 0.5 - 50;
-  let maxWidth = windowWidth * 0.9 * 0.5 - 50;
-
-  let minHeight = windowHeight * 0.5 * -0.5;
-  let minWidth = windowWidth * 0.9 * -0.5;
-
-  let [rerender, setRerender] = React.useState(true);
-  let [rerenderInterval, setRerenderInterval] = React.useState(true);
-  let [dice, setDice] = React.useState({
-    diceMaxRoll: 6,
-    dices: [
-      {
-        positionX: maxWidth,
-        positionY: maxHeight,
-        value: 1,
-        changingX: 10,
-        changingY: 10,
-      },
-      {
-        positionX: minWidth,
-        positionY: minHeight,
-        value: 1,
-        changingX: 10,
-        changingY: 10,
-      },
-    ],
-  });
-
-  let roll = () => {
-    for (let i = 0; i < dice.dices.length; i++) {
-      dice.dices[i].value = Math.ceil(Math.random() * dice.diceMaxRoll);
-    }
-    let intervalFunction = () => {
-      for (let i = 0; i < dice.dices.length; i++) {
-        if (dice.dices[i].positionX >= maxWidth) {
-          dice.dices[i].changingX = Math.ceil(Math.random() * 3) * -3;
-        }
-        if (dice.dices[i].positionX <= minWidth) {
-          dice.dices[i].changingX = Math.ceil(Math.random() * 3) * 3;
-        }
-        if (dice.dices[i].positionY >= maxHeight) {
-          dice.dices[i].changingY = Math.ceil(Math.random() * 3) * -3;
-        }
-        if (dice.dices[i].positionY <= minHeight) {
-          dice.dices[i].changingY = Math.ceil(Math.random() * 3) * 3;
-        }
-        let y = 0;
-        if (dice.dices[i + 1] !== undefined) {
-          y = i;
-        } else {
-          y = 0;
-        }
-        if (
-          dice.dices[y].positionX >= dice.dices[y + 1].positionX - 50 &&
-          dice.dices[y].positionX <= dice.dices[y + 1].positionX + 50 &&
-          dice.dices[y].positionY >= dice.dices[y + 1].positionY - 50 &&
-          dice.dices[y].positionY <= dice.dices[y + 1].positionY + 50
-        ) {
-          dice.dices[y].changingX = dice.dices[y].changingX * -1;
-          dice.dices[y + 1].changingX = dice.dices[y + 1].changingX * -1;
-          dice.dices[y].changingY = dice.dices[y].changingY * -1;
-          dice.dices[y + 1].changingY = dice.dices[y + 1].changingY * -1;
-        }
-        dice.dices[i].positionX =
-          dice.dices[i].positionX + dice.dices[i].changingX;
-        dice.dices[i].positionY =
-          dice.dices[i].positionY + dice.dices[i].changingY;
-        setDice(dice);
-        setRerenderInterval(Math.random());
-      }
-    };
-    setInterval(intervalFunction, 30);
-    setDice(dice);
-    setRerender(!rerender);
-  };
-
   return (
     <View style={styles.container}>
-      <View style={styles.board}>
-        {dice.dices.map((x) => (
-          <View style={{ left: x.positionX, top: x.positionY }}>
-            <View style={styles.dice}>
-              <Text>{x.value}</Text>
-            </View>
-          </View>
-        ))}
-      </View>
-      <Button title="Roll" onPress={roll}></Button>
-      <Text>
-        {windowHeight} {windowWidth}
-      </Text>
+      <Text>{"Hello Worlds"}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -105,26 +14,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "grey",
+    backgroundColor: "fff",
     alignItems: "center",
     justifyContent: "space-evenly",
-  },
-  board: {
-    height: "50%",
-    width: "90%",
-    backgroundColor: "white",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  dice: {
-    position: "absolute",
-    height: 50,
-    width: 50,
-    backgroundColor: "green",
-    alignItems: "center",
-    justifyContent: "center",
-    borderWidth: 2,
-    borderColor: "#20232a",
-    borderRadius: 6,
   },
 });
