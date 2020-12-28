f338859a1708dcc95a3f683afc3329d31f5a19ad
Marcin Medryk <medryk.kontakt@gmail.com>
2020-12-28 03:42:28 +0100

    wip

--- App.js
+++ App.js
@@ -11,36 +11,53 @@ import {
 } from "react-native-chart-kit";
 
 export default function App() {
+  let [year2019, setYear2019] = React.useState([
+    "01.01",
+    "02.01",
+    "03.01",
+    "04.01",
+    "05.01",
+    "06.01",
+    "07.01",
+    "08.01",
+    "09.01",
+    "10.01",
+  ]);
+
+  let [rerender, setRerender] = React.useState(true);
+  let [stockData, setStockData] = React.useState({
+    day: year2019.slice(0, 5),
+    price: [
+      Math.random() * 100,
+      Math.random() * 100,
+      Math.random() * 100,
+      Math.random() * 100,
+      Math.random() * 100,
+    ],
+  });
+
+  let nextDay = () => {
+    stockData.day = stockData.day.splice(1, 5);
+    stockData.price = stockData.price.splice(1, 5);
+    stockData.day.push(year2019.splice(stockData.day.length + 1, 1));
+    stockData.price.push(Math.random() * 100);
+    setStockData(stockData);
+    setRerender(!rerender);
+  };
+
   return (
     <View style={styles.container}>
       <View>
-        <Text>Bezier Line Chart</Text>
         <LineChart
           data={{
-            labels: [
-              "January",
-              "February",
-              "March",
-              "April",
-              "May",
-              "June",
-              "XXX",
-            ],
+            labels: stockData.day,
             datasets: [
               {
-                data: [
-                  Math.random() * 100,
-                  Math.random() * 100,
-                  Math.random() * 100,
-                  Math.random() * 100,
-                  Math.random() * 100,
-                  Math.random() * 100,
-                  Math.random() * 100,
-                ],
+                data: stockData.price,
               },
             ],
           }}
-          width={Dimensions.get("window").width} // from react-native
+          width={Dimensions.get("window").width - 10} // from react-native
           height={220}
           yAxisLabel="$"
           yAxisInterval={1} // optional, defaults to 1
@@ -67,6 +84,7 @@ export default function App() {
           }}
         />
       </View>
+      <Button title="Next Day" onPress={nextDay} />
     </View>
   );
 }
@@ -74,7 +92,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "fff",
+    backgroundColor: "white",
     alignItems: "center",
     justifyContent: "space-evenly",
   },
